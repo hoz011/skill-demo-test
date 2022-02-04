25f49ccef38d3c5135af12b248bd431a823354ec
hoz011 <97712975+hoz011@users.noreply.github.com>
2022-02-04 14:19:15 -0800

    Update

--- testerFileTest.java
+++ testerFileTest.java
@@ -2,8 +2,6 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class testerFileTest {
-    
-    //Test should fail
     @Test
     public void testIsEven() {
         assertEquals(true, testerFile.isEvenSum(4, 2));
